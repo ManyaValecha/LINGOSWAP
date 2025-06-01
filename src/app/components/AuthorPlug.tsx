c05cbcadbd9b36b6d20c2636b16e1dee4b07fcf5
Kanak Maheshwari <140155806+kanakmaheshwari3115@users.noreply.github.com>
2025-06-01 19:28:06 +0530

    Update AuthorPlug.tsx

--- src/app/components/AuthorPlug.tsx
+++ src/app/components/AuthorPlug.tsx
@@ -4,18 +4,12 @@ import { FaGithub } from "react-icons/fa";
 export default function AuthorPlug() {
     return (
             <div className="w-full text-[9px] mt-2 flex items-center gap-1">
-                <span className="text-[rgb(150,150,150)]">MADE WITH ❤️ BY</span>
-                <Link
-                    className="underline text-blue-500 hover:text-purple-500 transition"
-                    href={"https://alvinchang.dev"}
-                >
-                    AURORA
-                </Link>
-
+                <span className="text-[rgb(150,150,150)]">MADE WITH ❤️ BY KANAK & MANYA,</span>
+                
                 <span className="text-[rgb(150,150,150)]">CLONE THE PROJECT ON</span>
                 <Link
                     className="underline text-blue-500 hover:text-purple-500 transition"
-                    href={"https://github.com/auroradream04/genzify-app"}
+                    href={"https://github.com/ManyaValecha/LINGOSWAP"}
                 >
                     GITHUB
                 </Link>
